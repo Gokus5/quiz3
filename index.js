368ea6fd05adb66079ddf9e12caa60dc163095f8
Harrvvin Raj <you@example.comharrvvinr@gmail.com>
2023-04-12 14:29:39 +0800

    login

--- index.js
+++ index.js
@@ -1,3 +1,52 @@
+let dbuser = [
+
+
+  {
+      username:"harrvvin",
+      password:"123456",
+      name:"hr",
+      email:"harrvvin@utem.edu.my"
+  }
+  ,
+  {
+      username:"yuva",
+      password:"123789",
+      name:"yv",
+      email:"yuva@utem.edu.my"
+  }
+  ,
+  {
+      username:"ashwini",
+      password:"321456",
+      name:"ash",
+      email:"ashwini@utem.edu.my"
+  }
+  ]
+  
+  function login (reqUsername, reqPassword){
+      let matchUser = dbuser.find (
+          user => user.username == reqUsername
+      )
+      console.log(matchUser)
+  }
+
+  function register (reqUsername,reqPassword,reqName,reqEmail)
+  {
+    dbUsers.push({
+        username: reqUsername,
+        password: reqPassword,
+        name: reqName,
+        email: reqEmail
+
+    })
+
+  }
+
+  
+//try to login
+login("harrvvin","password")
+login("utem","password")
+  
 const express = require('express')
 const app = express()
 const port = 3000
@@ -7,7 +56,9 @@ app.use(express.json())
 app.post('/login', (req, res) => {
     console.log(req.body)
 
-    res.send('Login')
+    let result = login (req.body.username,req.body.password)
+
+    res.send(result)
   })
 
 app.get('/', (req, res) => {
@@ -19,7 +70,13 @@ app.get('/bye', (req, res) => {
   })
 
   app.post('/register', (req, res) => {
-    res.send('Account Created!')
+    let result = register (
+    req.body.username,
+    req.body.password,
+    req.body.name,
+    req.body.email
+    )
+    res.send(result)
   })
 
 app.listen(port, () => {
